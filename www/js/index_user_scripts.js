200823a1aca4414c96c466b6bde0b3aa6b2d3093
Doug Stevens <StevensD@fsd38.ab.ca>
2016-06-23 09:01:59 -0600

    Adding upload capabilities

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -61,14 +61,68 @@
         
 //var snotes;
         var sname=$("#nameID").val();
-        //snotes=document.getElementsById("snotes");
-        //var snamed=new FormData(sname);
-        alert("<body>"+sname+"...</body>");
-        $("#locID").val(sname);
-        //activate_page("#displaydata"); 
+        var sloc=$("#locID").val();
+        var stime=$("#timeID").val();
+        var snotes=$("#notesID").val();
+        var sincident=$("#incidentID").val();
+                var taskProperties = {
+            'TaskName': 'Order Approval',
+            'AssignedToId': 12
+        };
+
+       createListItem('https://connect.fsd38.ab.ca/FSDConnect','SRSIncidents',taskProperties,function(task){
+            console.log('Task' + task.TaskName + ' has been created'); 
+          },
+          function(error){
+            $("#runout").val(JSON.stringify(error));
+          }
+        );
+
+        document.getElementById("nameOUT").innerHTML=sname;
+        document.getElementById("locOUT").innerHTML=sloc;
+        document.getElementById("timeOUT").innerHTML=stime;
+        document.getElementById("noteOUT").innerHTML=snotes;
+        document.getElementById("incidentOUT").innerHTML=sincident;
+        activate_page("#displaydata"); 
          return false;
     }); 
+    
+        /* button  Return */
+    $(document).on("click", ".uib_w_16", function(evt)
+    {
+        /* your code goes here */ 
+        //Usage: create task
+        $("#nameID").val("");
+        $("#locID").val("");
+        $("#timeID").val("");
+        $("#notesID").val("");
+        $("#incidentID").val("");
+        activate_page("#mainpage");
+         return false;
+    });
     
     }
  document.addEventListener("app.Ready", register_event_handlers, false);
 })();
+
+function createListItem(webUrl,listName, itemProperties, success, failure) {
+    $("#runout").val("Here");
+    $.ajax({
+        url: webUrl + "/_vti_bin/listdata.svc/" + listName,
+        type: "POST",
+        processData: false,
+        contentType: "application/json;odata=verbose",
+        data: JSON.stringify(itemProperties),
+        headers: {
+            "Accept": "application/json;odata=verbose"
+        },
+        success: function (data) {
+            success(data.d);
+        },
+        error: function (data) {
+            failure(data.responseJSON.error);
+        }
+    });
+}
+
+
